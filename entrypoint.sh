ee5e5561a291be0c9b920d4107f0b65d00964b3e
BenTheElder <bentheelder@gmail.com>
2017-12-23 21:47:20 -0800

    tweak 404 and image DNS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -x
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf && cat /etc/resolv.conf
 # kubernetes only resolves internal services by default
 git clone https://github.com/BenTheElder/site &&
 cd site &&
--- push_image.sh
+++ push_image.sh
@@ -5,7 +5,7 @@ DOCKER_ID_USER="${DOCKER_ID_USER:-bentheelder}"
 # build binary
 env GOOS=linux go build . && \
 # build, tag and push image
-docker build -t "${IMAGE_NAME}" . && \
+docker build --no-cache -t "${IMAGE_NAME}" . && \
 docker tag "${IMAGE_NAME}" "${DOCKER_ID_USER}/${IMAGE_NAME}"
 docker push "${DOCKER_ID_USER}/${IMAGE_NAME}"
 # cleanup
