a99883a6137627f6ceb7a5765e36db7d4e967b38
BenTheElder <bentheelder@gmail.com>
2017-12-25 17:03:55 -0800

    fixup too-many-lasers url

--- www/projects/too-many-lasers/js/game.js
+++ www/projects/too-many-lasers/js/game.js
@@ -100,7 +100,7 @@ var Game;
 (function (Game) {
     class Boot extends Phaser.State {
         preload() {
-            this.load.image("preloadBar", "assets/loader.png");
+            this.load.image("preloadBar", "too-many-lasers/assets/loader.png");
             this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         }
         create() {
@@ -583,46 +583,46 @@ var Game;
             this.load.setPreloadSprite(this.preloadBar);
             this.preloadBar.scale.setTo(4, 4);
             // Load assets here
-            this.load.image("background", "assets/Backgrounds/darkPurple.png");
+            this.load.image("background", "too-many-lasers/assets/Backgrounds/darkPurple.png");
             // fighters
-            this.load.image("fighter_1", "assets/playerShip1_green.png");
-            this.load.image("fighter_2", "assets/playerShip1_red.png");
-            this.load.image("fighter_3", "assets/playerShip1_blue.png");
+            this.load.image("fighter_1", "too-many-lasers/assets/playerShip1_green.png");
+            this.load.image("fighter_2", "too-many-lasers/assets/playerShip1_red.png");
+            this.load.image("fighter_3", "too-many-lasers/assets/playerShip1_blue.png");
             // this.load.image("fighter_4", "assets/playerShip1_orange.png");
             // cruisers
-            this.load.image("cruiser_1", "assets/playerShip2_green.png");
-            this.load.image("cruiser_2", "assets/playerShip2_red.png");
-            this.load.image("cruiser_3", "assets/playerShip2_blue.png");
+            this.load.image("cruiser_1", "too-many-lasers/assets/playerShip2_green.png");
+            this.load.image("cruiser_2", "too-many-lasers/assets/playerShip2_red.png");
+            this.load.image("cruiser_3", "too-many-lasers/assets/playerShip2_blue.png");
             // this.load.image("cruiser_4", "assets/playerShip2_orange.png");
             // battleships
-            this.load.image("battleship_1", "assets/playerShip3_green.png");
-            this.load.image("battleship_2", "assets/playerShip3_red.png");
-            this.load.image("battleship_3", "assets/playerShip3_blue.png");
+            this.load.image("battleship_1", "too-many-lasers/assets/playerShip3_green.png");
+            this.load.image("battleship_2", "too-many-lasers/assets/playerShip3_red.png");
+            this.load.image("battleship_3", "too-many-lasers/assets/playerShip3_blue.png");
             // this.load.image("battleship_4", "assets/playerShip3_orange.png");
             // bullets
-            this.load.image("bullet_1", "assets/Lasers/laserGreen05.png");
-            this.load.image("bullet_2", "assets/Lasers/laserRed03.png");
-            this.load.image("bullet_3", "assets/Lasers/laserBlue03.png");
+            this.load.image("bullet_1", "too-many-lasers/assets/Lasers/laserGreen05.png");
+            this.load.image("bullet_2", "too-many-lasers/assets/Lasers/laserRed03.png");
+            this.load.image("bullet_3", "too-many-lasers/assets/Lasers/laserBlue03.png");
             // NOTE: orange lasers do not exist currently...
             // this.load.image("bullet_4", "assets/Lasers/laserOrange03.png");
             // Load damage overlays
-            this.load.image("fighter_damage_1", "assets/Damage/playerShip1_damage1.png");
-            this.load.image("fighter_damage_2", "assets/Damage/playerShip1_damage2.png");
-            this.load.image("fighter_damage_3", "assets/Damage/playerShip1_damage3.png");
-            this.load.image("cruiser_damage_1", "assets/Damage/playerShip2_damage1.png");
-            this.load.image("cruiser_damage_2", "assets/Damage/playerShip2_damage2.png");
-            this.load.image("cruiser_damage_3", "assets/Damage/playerShip2_damage3.png");
-            this.load.image("battleship_damage_1", "assets/Damage/playerShip3_damage1.png");
-            this.load.image("battleship_damage_2", "assets/Damage/playerShip3_damage2.png");
-            this.load.image("battleship_damage_3", "assets/Damage/playerShip3_damage3.png");
+            this.load.image("fighter_damage_1", "too-many-lasers/assets/Damage/playerShip1_damage1.png");
+            this.load.image("fighter_damage_2", "too-many-lasers/assets/Damage/playerShip1_damage2.png");
+            this.load.image("fighter_damage_3", "too-many-lasers/assets/Damage/playerShip1_damage3.png");
+            this.load.image("cruiser_damage_1", "too-many-lasers/assets/Damage/playerShip2_damage1.png");
+            this.load.image("cruiser_damage_2", "too-many-lasers/assets/Damage/playerShip2_damage2.png");
+            this.load.image("cruiser_damage_3", "too-many-lasers/assets/Damage/playerShip2_damage3.png");
+            this.load.image("battleship_damage_1", "too-many-lasers/assets/Damage/playerShip3_damage1.png");
+            this.load.image("battleship_damage_2", "too-many-lasers/assets/Damage/playerShip3_damage2.png");
+            this.load.image("battleship_damage_3", "too-many-lasers/assets/Damage/playerShip3_damage3.png");
             // Walls can be visible or invisible
-            this.load.image("red-pixel", "assets/red-pixel.png");
-            this.load.image("transparent-pixel", "assets/transparent-pixel.png");
-            this.load.image("play", "assets/play.png");
+            this.load.image("red-pixel", "too-many-lasers/assets/red-pixel.png");
+            this.load.image("transparent-pixel", "too-many-lasers/assets/transparent-pixel.png");
+            this.load.image("play", "too-many-lasers/assets/play.png");
             // load laser sounds
-            this.load.audio("fighter_fire", "assets/Sounds/SoundsCrate-SciFi-Laser4.mp3");
-            this.load.audio("cruiser_fire", "assets/Sounds/SoundsCrate-SciFi-Laser1.mp3");
-            this.load.audio("battleship_fire", "assets/Sounds/SoundsCrate-SciFi-Laser5.mp3");
+            this.load.audio("fighter_fire", "too-many-lasers/assets/Sounds/SoundsCrate-SciFi-Laser4.mp3");
+            this.load.audio("cruiser_fire", "too-many-lasers/assets/Sounds/SoundsCrate-SciFi-Laser1.mp3");
+            this.load.audio("battleship_fire", "too-many-lasers/assets/Sounds/SoundsCrate-SciFi-Laser5.mp3");
         }
         create() {
             const pressKeyText = "Click anywhere to start...";
