7db89fe9c624d108908e82df7cfa4f8ee0e687fb
BenTheElder <bentheelder@gmail.com>
2018-01-15 01:13:22 -0800

    deprecate repo

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -x
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf && cat /etc/resolv.conf
-# switch to workdir and delete anything left behind
-cd "${WORKSPACE}" && find -mindepth 1 -delete
-# kubernetes only resolves internal services by default
-git clone https://github.com/BenTheElder/site &&
-cd site &&
-site
--- on_hook.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -xv
-if [[ ! -z "${DEFINITELY_RUNNING_IN_PRODUCTION}" ]]; then
-    BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    git fetch origin && git reset --hard origin/$BRANCH
-fi
-./www/posts/update.sh
